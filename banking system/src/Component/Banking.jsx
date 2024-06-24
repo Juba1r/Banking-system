3422b207840fe73d0bc0651a2dc74669d4f4d9d1
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-06-25 03:03:26 +0600

    till submit button

--- banking system/src/Component/Banking.jsx
+++ banking system/src/Component/Banking.jsx
@@ -4,24 +4,26 @@ import "./Banking.css";
 const Banking = () => {
   const [balance, setBalance] = useState(0);
   const [amount, setAmount] = useState("");
-  
+  const [showD, setShowD] = useState(true);
+  const [showE, setShowE] = useState(true);
 
   function Deposit() {
     const depositBy = parseFloat(amount);
     if (depositBy > 0) {
       setBalance(balance + depositBy);
       setAmount("");
+      setShowD(true);
     }
   }
 
-  function expanse() {
+  function Expanse() {
     const expanseBy = parseFloat(amount);
     if (expanseBy > 0 && expanseBy <= balance) {
       setBalance(balance - expanseBy);
       setAmount("");
+      setShowE(true);
     }
   }
-
 
   return (
     <div className="banking-container">
@@ -29,68 +31,65 @@ const Banking = () => {
       <div className="current-balance">Current Balance: ${balance}</div>
       <div className="input-container">
         <div className="button-group">
-          <button className="button-52" onClick={Deposit}>
+          <button className="button-52" onClick={() => setShowD(false)}>
             Deposit
           </button>
-          <button className="button-52" onClick={expanse}>
+          <button className="button-52" onClick={() => setShowE(false)}>
             Expanse
           </button>
-        
         </div>
 
-        <div className="inptmodify-deposit">
-        <h2>Deposit</h2>
-          <div className="name-container">
-            <label htmlFor="Name">Name : </label>
+        {!showD &&  (
+          <div className="inptmodify-deposit">
+            <h2>Deposit</h2>
+            <div className="name-container">
+              <label htmlFor="Name">Name : </label>
 
-            <input type="Name" placeholder="Enter the reason" />
+              <input type="Name" placeholder="Enter the reason" />
+            </div>
+
+            <div className="amount-container">
+              <label htmlFor="amount">Amount : </label>
+
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter amount"
+              />
+            </div>
+
+            <button className="submit-btn" onClick={Deposit}>Submit</button>
           </div>
+        )}
 
-          <div className="amount-container">
-            <label htmlFor="amount">Amount : </label>
+        {!showE &&  (
+          <div className="inptmodify-expanse">
+            <h2>Expanse</h2>
 
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="Enter amount"
-            />
+            <div className="name-container">
+              <label htmlFor="Name">Name : </label>
+
+              <input type="Name" placeholder="Enter the reason" />
+            </div>
+
+            <div className="amount-container">
+              <label htmlFor="amount">Amount : </label>
+
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter amount"
+              />
+            </div>
+
+            <button className="submit-btn" onClick={Expanse}>Submit</button>
           </div>
-
-          <button className="submit-btn">Submit</button>
-
-
-        </div>
-        <div className="inptmodify-expanse">
-
-          <h2>Expanse</h2>
-
-          <div className="name-container">
-            <label htmlFor="Name">Name : </label>
-
-            <input type="Name" placeholder="Enter the reason" />
-          </div>
-
-          <div className="amount-container">
-            <label htmlFor="amount">Amount : </label>
-
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="Enter amount"
-            />
-          </div>
-
-          <button className="submit-btn">Submit</button>
-
-
-        </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Banking;
-
-
