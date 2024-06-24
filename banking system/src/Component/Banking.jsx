dabd0bc208af4f176595fa08e56294b01d547ef9
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-06-24 13:15:12 +0600

    update

--- banking system/src/Component/Banking.jsx
+++ banking system/src/Component/Banking.jsx
@@ -1,45 +1,51 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import './Banking.css';
 
 const Banking = () => {
-  const [balance, setBalance] = useState(0)
-
-  const [amount, setAmount] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [amount, setAmount] = useState('');
 
   function Deposit() {
-    const depositBy = parseFloat(amount)
+    const depositBy = parseFloat(amount);
     if (depositBy > 0) {
-      setBalance(balance + depositBy)
-      setAmount('')
-    } 
+      setBalance(balance + depositBy);
+      setAmount('');
+    }
   }
-0
+
   function expanse() {
     const expanseBy = parseFloat(amount);
     if (expanseBy > 0 && expanseBy <= balance) {
-      setBalance(balance - expanseBy)
-      setAmount('')
+      setBalance(balance - expanseBy);
+      setAmount('');
     }
   }
 
   return (
     <div className="banking-container">
-
       <h1>Banking System</h1>
-
-        <h3>Current Balance: ${balance}</h3>
-        <br />
-
+      <div className="current-balance">Current Balance: ${balance}</div>
       <div className="input-container">
+        <div className="button-group">
+          <button className="button-52" onClick={Deposit}>Deposit</button>
+          <button className="button-52" onClick={expanse}>Expanse</button>
+        </div>
 
-        
-
-        <button className='button-48' onClick={Deposit}>Deposit</button> 
-        <button className='button-48' onClick={expanse}>Expanse</button>
+        <div className="inptmodify">
 
 
-        
+        <div className="name-container">
 
+<label htmlFor="Name">Name : </label>
+<br />
+
+<input type="Name" placeholder='Enter the reason'/>
+</div>
+          
+          <div className="amount-container">
+
+          <label htmlFor="amount">Amount : </label>
+          <br />
 
         <input
           type="number"
@@ -47,15 +53,12 @@ const Banking = () => {
           onChange={(e) => setAmount(e.target.value)}
           placeholder="Enter amount"
         />
+          </div>
 
-
-
+        </div>
       </div>
-
-
-
     </div>
-  )
+  );
 }
 
 export default Banking;
